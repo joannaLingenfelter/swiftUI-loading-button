d4271b7fb6c70a6380d4deace473e93a24d80286
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2022-07-04 15:17:14 -0400

    Initial Commit

---
+++ swiftUI-loading-button/swiftUI_loading_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_loading_buttonApp.swift
+//  swiftUI-loading-button
+//
+//  Created by Joanna Lingenfelter on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_loading_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
