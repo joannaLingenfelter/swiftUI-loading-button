f61527acbba0ea0b38f6a5ad2dd06caacc7e5a8d
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2022-07-05 10:24:15 -0400

    Implement Loading Button with wrapped view and with a ViewModifier

--- swiftUI-loading-button/ContentView.swift
+++ swiftUI-loading-button/ContentView.swift
@@ -6,11 +6,116 @@
 //
 
 import SwiftUI
+import Combine
+
+protocol LoadingButtonStyle: ButtonStyle {
+    init(isLoading: Bool)
+}
+
+
+struct LoadingButton<ButtonStyle: LoadingButtonStyle, Content: View>: View {
+    let buttonStyle: ButtonStyle.Type
+    let action: () -> Void
+    let label: () -> Content
+
+    @Binding private var isLoading: Bool
+
+    init(buttonStyle: ButtonStyle.Type, isLoading: Binding<Bool>, action: @escaping () -> Void, label: @escaping () -> Content) {
+        self.buttonStyle = buttonStyle
+        _isLoading = isLoading
+        self.action = action
+        self.label = label
+    }
+
+    var body: some View {
+        Button(action: action, label: label)
+            .buttonStyle(buttonStyle.init(isLoading: isLoading))
+    }
+}
+
+struct MyButtonStyle: LoadingButtonStyle {
+    let isLoading: Bool
+
+    func makeBody(configuration: ButtonStyleConfiguration) -> some View {
+        return configuration.label
+        .padding(.vertical)
+        .padding(.horizontal, 20)
+        .foregroundColor(.white)
+        .background(Color.blue)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .opacity(isLoading ? 0.5 : 1.0)
+        .overlay(
+            Color.black
+                .opacity(configuration.isPressed ? 0.2 : 0.0)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        )
+        .overlay {
+            if isLoading {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+            }
+        }
+    }
+}
+
+struct Loader: ViewModifier {
+    @Binding var isLoading: Bool
+
+    func body(content: Content) -> some View {
+        content
+            .opacity(isLoading ? 0.5: 1.0)
+            .overlay {
+                if isLoading {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                }
+            }
+
+    }
+}
+
+extension View {
+    func loading(_ bool: Binding<Bool>) -> some View {
+        modifier(Loader(isLoading: bool))
+    }
+}
 
 struct ContentView: View {
+    @State var button1IsLoading: Bool = false
+    @State var button2IsLoading: Bool = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            LoadingButton(buttonStyle: MyButtonStyle.self, isLoading: $button1IsLoading) {
+                withAnimation(.easeInOut) {
+                    button1IsLoading.toggle()
+                }
+                print("Pressed!")
+            } label: {
+                Text("Press me!")
+            }
+
+            Button("Press me!") {
+                withAnimation(.easeInOut) {
+                    button2IsLoading.toggle()
+                }
+            }
+            .padding(.vertical)
+            .padding(.horizontal, 20)
+            .foregroundColor(.white)
+            .background(Color.green)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .loading($button2IsLoading)
+        }
+
+        Button("Press me!") {
+
+        }
+        .padding(.vertical)
+        .padding(.horizontal, 20)
+        .foregroundColor(.white)
+        .background(Color.red)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
